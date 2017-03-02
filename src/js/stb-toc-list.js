55a1951577aa5ff4e9676642de0f0b97a26ffb15
Ksawery Buczkowski <ksawery.buczkowski@kantega.no>
2017-03-02 18:10:25 +0100

    DS-1412 when toc-hidden class is present in the toc-header elements, don’t show them in toc-list.

--- src/js/stb-toc-list.js
+++ src/js/stb-toc-list.js
@@ -22,22 +22,22 @@
 
   $(document).ready(function() {
     // Prepare the row that contains the table of contents
-    var tocList ='<div class="row"><div class="col-md-8 toc-list top-padding-10 border-top-and-bottom stbcolor-background fifth"><p class="intro"><a href="#" onclick="$(\'.toc-list .items\').slideToggle();$(\'.toc-list p span\').toggleClass(\'chevron-down red chevron-up\');return false;">G&aring; direkte til<span class="stb-sprite-medium chevron-up red pull-right"></a></span></p><ul class="items"></ul></div></div>';
+    var tocList ='<div class="row"><div class="col-md-8 toc-list top-padding-10 border-top-and-bottom stbcolor-background fifth"><p class="intro"><a href="#" onclick="$(\'.toc-list .items\').slideToggle();$(\'.toc-list p span\').toggleClass(\'chevron-down red chevron-up\');return false;">G&aring; direkte til<span class="stb-sprite-medium chevron-down pull-right"></a></span></p><ul class="items"></ul></div></div>';
 
     // Place the table of content above the first hx.toc-header
-    if($('h2.toc-header').length===0) {
-      $(tocList).insertBefore( $('h3.toc-header:first').closest("div.row") );
+    if($('h2.toc-header:not(.toc-hidden)').length===0) {
+      $(tocList).insertBefore( $('h3.toc-header:not(.toc-hidden):first').closest("div.row") );
     } else {
       // When we don't have to support h3-based toc any more, we can replace this if sentence with just the following line
-      $(tocList).insertBefore( $('h2.toc-header:first').closest("div.row") );
+      $(tocList).insertBefore( $('h2.toc-header:not(.toc-hidden):first').closest("div.row") );
     }
 
     // Find all the top level TOC headings (h2)
-    var tocHeaders= $('h2.toc-header');
+    var tocHeaders= $('h2.toc-header:not(.toc-hidden)');
     // Small more or less temporary fix, because we moved the tocHeaders from h3 to h2
     var tocMode = "h2";
     if (tocHeaders.length===0) {
-      tocHeaders = $('h3.toc-header');
+      tocHeaders = $('h3.toc-header:not(.toc-hidden)');
       tocMode = "h3";
     }
 
